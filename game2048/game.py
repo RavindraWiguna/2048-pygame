f4a0ca44e1da295c8a3bb3db962fd36d8b3523a3
RavindraWiguna <ravindrawiguna@gmail.com>
2022-04-24 20:52:21 +0700

    finish 2048, but da ui is still bad, but it'll do for the moment

--- game2048/game.py
+++ game2048/game.py
@@ -1,4 +1,6 @@
+from collections import Counter
 from copy import deepcopy
+import numpy as np
 import pygame
 from colorData import WHITE, GRAY
 from random import choice
@@ -8,8 +10,11 @@ from math import sqrt
 pygame.init()
 
 class GameInformation:
-    def __init__(self):
-        pass
+    def __init__(self, matrix, isGameOver, score):
+        self.Matrix = matrix
+        self.isGameOver = isGameOver
+        self.score = score
+        
 
 class Game:
     # Game setup
@@ -22,89 +27,136 @@ class Game:
         self.unit_w =self.window_width//4
         self.unit_h = self.window_height//4
         self.Matrix = [0, 0, 0, 0, 
-                       2, 2, 0, 0,
+                       0, 0, 0, 0,
                        2, 0, 0, 0,
-                       0, 0, 0, 0]
+                       2, 0, 0, 0]
         self.is_pressed = False                 # For human player, so that it only move after not is pressed
         self.last_move = None                   # also for human player to check if we release the last button
         self.empty_tile = []                    # store all 0 in the matrix to spawn random '2' or '4'
         self.done_move =False                   # safe guard so that we didn't spawn before moving
+        self.score = 0
 
     def up(self):
-        isUpdating = True
-        everCombine = False
-        while(isUpdating):
-            last_state = deepcopy(self.Matrix)
-            for col in range(4):
+        availableForCombine = Counter()         # store id of the matrix where that id's value is the result of combining 2 similar value
+        for col in range(4):
+            change = 1                          # initialize the change as 1 so that the loop get executed at least once
+            while(change > 0):
+                change = 0
                 for row in range(1, 4):
                     cur_id = row*4 + col
+                    if(self.Matrix[cur_id] == 0):
+                        # skip empty tile
+                        continue
+                    
                     up_id = cur_id-4
-                    if(self.Matrix[up_id] == 0 or self.Matrix[up_id] == self.Matrix[cur_id]):
+                    if(self.Matrix[up_id] == 0):
                         # bisa naik ke atas
-                        everCombine = everCombine or self.Matrix[up_id] == self.Matrix[cur_id]
                         self.Matrix[up_id] += self.Matrix[cur_id]
                         self.Matrix[cur_id] = 0
-            if(self.Matrix == last_state):
-                isUpdating = False
-    
+                        change +=1
+                    elif(self.Matrix[up_id] == self.Matrix[cur_id] and availableForCombine[up_id]==0 and availableForCombine[cur_id] == 0):
+                        # kalau tidak 0 dan sama dan bisa di combine
+                        change +=1
+                        self.Matrix[up_id] += self.Matrix[cur_id]
+                        self.Matrix[cur_id] = 0
+                        availableForCombine[up_id] = 1 # 1 means not available (yea not so intuitive but there exist a thing called comment and reading)
+                        self.score+=self.Matrix[up_id]    
+
     def down(self):
-        isUpdating = True
-        while(isUpdating):
-            last_state = deepcopy(self.Matrix)
-            for col in range(4):
+        availableForCombine = Counter()         # store id of the matrix where that id's value is the result of combining 2 similar value
+        for col in range(4):
+            change = 1                          # initialize the change as 1 so that the loop get executed at least once
+            while(change > 0):
+                change = 0
                 for row in range(2, -1, -1):
                     cur_id = row*4 + col
+                    if(self.Matrix[cur_id] == 0):
+                        # skip empty tile
+                        continue
+                    
                     down_id = cur_id+4
-                    if(self.Matrix[down_id] == 0 or self.Matrix[down_id] == self.Matrix[cur_id]):
-                        # bisa naik ke atas
+                    if(self.Matrix[down_id] == 0):
+                        # bisa ke bawah
                         self.Matrix[down_id] += self.Matrix[cur_id]
                         self.Matrix[cur_id] = 0
-            if(self.Matrix == last_state):
-                isUpdating = False
+                        change +=1
+                    elif(self.Matrix[down_id] == self.Matrix[cur_id] and availableForCombine[down_id]==0 and availableForCombine[cur_id] == 0):
+                        # kalau tidak 0 dan sama dan bisa di combine
+                        change +=1
+                        self.Matrix[down_id] += self.Matrix[cur_id]
+                        self.Matrix[cur_id] = 0
+                        availableForCombine[down_id] = 1 # 1 means not available (yea not so intuitive but there exist a thing called comment and reading)
+                        self.score+=self.Matrix[down_id]
 
     def left(self):
-        isUpdating = True
-        while(isUpdating):
-            last_state = deepcopy(self.Matrix)
-            for row in range(4):
+        availableForCombine = Counter()         # store id of the matrix where that id's value is the result of combining 2 similar value
+        for row in range(4):
+            change = 1                          # initialize the change as 1 so that the loop get executed at least once
+            while(change > 0):
+                change = 0
                 for col in range(1, 4):
                     cur_id = row*4 + col
+                    if(self.Matrix[cur_id] == 0):
+                        # skip empty tile
+                        continue
+                    
                     left_id = cur_id-1
-                    if(self.Matrix[left_id] == 0 or self.Matrix[left_id] == self.Matrix[cur_id]):
-                        # bisa naik ke atas
+                    if(self.Matrix[left_id] == 0):
+                        # bisa ke kiri
                         self.Matrix[left_id] += self.Matrix[cur_id]
                         self.Matrix[cur_id] = 0
-            if(self.Matrix == last_state):
-                isUpdating = False
+                        change +=1
+                    elif(self.Matrix[left_id] == self.Matrix[cur_id] and availableForCombine[left_id]==0 and availableForCombine[cur_id] == 0):
+                        # kalau tidak 0 dan sama dan bisa di combine
+                        change +=1
+                        self.Matrix[left_id] += self.Matrix[cur_id]
+                        self.Matrix[cur_id] = 0
+                        availableForCombine[left_id] = 1 # 1 means not available (yea not so intuitive but there exist a thing called comment and reading)
+                        self.score+=self.Matrix[left_id]
 
     def right(self):
-        isUpdating = True
-        while(isUpdating):
-            last_state = deepcopy(self.Matrix)
-            for row in range(4):
+        availableForCombine = Counter()         # store id of the matrix where that id's value is the result of combining 2 similar value
+        for row in range(4):
+            change = 1                          # initialize the change as 1 so that the loop get executed at least once
+            while(change > 0):
+                change = 0
                 for col in range(2, -1, -1):
                     cur_id = row*4 + col
+                    if(self.Matrix[cur_id] == 0):
+                        # skip empty tile
+                        continue
+                    
                     right_id = cur_id+1
-                    if(self.Matrix[right_id] == 0 or self.Matrix[right_id] == self.Matrix[cur_id]):
-                        # bisa naik ke atas
+                    if(self.Matrix[right_id] == 0):
+                        # bisa ke kanan
                         self.Matrix[right_id] += self.Matrix[cur_id]
                         self.Matrix[cur_id] = 0
-            if(self.Matrix == last_state):
-                isUpdating = False
+                        change +=1
+                    elif(self.Matrix[right_id] == self.Matrix[cur_id] and availableForCombine[right_id]==0 and availableForCombine[cur_id] == 0):
+                        # kalau tidak 0 dan sama dan bisa di combine
+                        change +=1
+                        self.Matrix[right_id] += self.Matrix[cur_id]
+                        self.Matrix[cur_id] = 0
+                        availableForCombine[right_id] = 1 # 1 means not available (yea not so intuitive but there exist a thing called comment and reading)
+                        self.score+=self.Matrix[right_id]
 
+    # will update the matrix, boolean indicating a move has been done and the score
     def move(self, direction):
+        last_state = deepcopy(self.Matrix)
         if(direction=="U"):
             self.up()
-            self.done_move = True
         elif(direction=="D"):
             self.down()
-            self.done_move = True
         elif(direction=='L'):
             self.left()
-            self.done_move = True
         elif(direction=="R"):
             self.right()
+        
+        if(self.Matrix == last_state):
+            self.done_move= False
+        else:
             self.done_move = True
+
 
 
     def draw_board(self):
@@ -124,20 +176,52 @@ class Game:
         self.empty_tile = [i for i, val in enumerate(self.Matrix) if val == 0]
 
 
+    # check for game over (no empty tiles and not a single combine is possible)
+    def check_game_over(self):
+        # Update the empty tiles
+        self.get_empty_tile()
+        # if exist at least one zero, then it aint the end of world
+        if(self.empty_tile):
+            return False
+        
+        # hm, so there is not empty tile..., let's check for combine-es (only the first 3 row and col needed to checked)
+        maybeGameOver = True
+        for row in range(3):
+            if(maybeGameOver):
+                for col in range(3):
+                    # check horizontal equalness
+                    cur_id = row * 4 + col
+                    if(self.Matrix[cur_id] == self.Matrix[cur_id+1]):
+                        maybeGameOver = False
+                        break
+                    elif(self.Matrix[cur_id] == self.Matrix[cur_id+4]):
+                        maybeGameOver=False
+                        break
+        return maybeGameOver
+
+
+
+        
+
+
     def loop(self):
         """
         Executes a single game loop.
         :returns: GameInformation instance
         """
-        game_info = GameInformation()
+        # check for game overness
+        gameOver = self.check_game_over()
 
+        # if a move has been done then we spawn
         if(self.done_move):
-            # get empty tile
-            self.get_empty_tile()
-            # spawn random tile
-            r_id = choice(self.empty_tile)
-            self.Matrix[r_id] = choice((2, 2, 4, 2, 2))
-            self.done_move = False
+            if(self.empty_tile):
+                print("spawn")
+                # spawn random tile
+                r_id = choice(self.empty_tile)
+                self.Matrix[r_id] = choice((2, 2, 2, 2, 2, 4, 2, 2, 2, 2))
+                self.done_move = False
+        
+        game_info = GameInformation(self.Matrix, gameOver, self.score)
         return game_info
 
     def reset(self):
@@ -150,6 +234,7 @@ if __name__=="__main__":
     pygame.display.set_caption("2048")
     the_game = Game(window, width, height)
     clock = pygame.time.Clock()
+    game_info = None
     while(isRunning):
         clock.tick(60)
         for event in pygame.event.get():
@@ -178,9 +263,11 @@ if __name__=="__main__":
         else:
             # is pressed
             if(not keys[the_game.last_move]):
+                print(game_info.score)
                 the_game.is_pressed = False
         
-        the_game.loop()
+        game_info = the_game.loop()
+        isRunning = not game_info.isGameOver
         the_game.draw()
         pygame.display.update()
 
